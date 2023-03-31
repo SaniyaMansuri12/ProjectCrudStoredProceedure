ec994aa76cbcd49df0b98a596794cc2be1a7b836
SaniyaMansuri12 <114173445+SaniyaMansuri12@users.noreply.github.com>
2023-03-31 08:58:20 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,124 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CollegeRegistrationWithCruDStoredProcedure.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    
+    <title>GridView Crud Operations using Stored Procedure in ASP.Net</title>  
+  
+    <style type="text/css">  
+        .GridviewDesign {  
+            font-size: 100%;  
+            font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;  
+            color: #303933;  
+        }  
+          
+        .headerstyleForGrid {  
+            color: #FFFFFF;  
+            border-right-color: #abb079;  
+            border-bottom-color: #abb079;  
+            background-color: #df5015;  
+            padding: 0.5em 0.5em 0.5em 0.5em;  
+            text-align: center;  
+        }  
+        .auto-style1 {
+            margin-left: 90px;
+            margin-top: 27px;
+        }
+    </style>  
+  
+</head>  
+  
+<body>  
+  
+    <form id="form1" runat="server">  
+  
+        <div class="GridviewDesign" style="font-size: large">  
+  
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; College Details Form  
+  
+            <asp:GridView runat="server" ID="gvDetailsForCurdOperation" ShowFooter="True" AllowPaging="True" AutoGenerateColumns="False" DataKeyNames="CollageId,Collagename" OnPageIndexChanging="gvDetailsForCurdOperation_PageIndexChanging" OnRowCancelingEdit="gvDetailsForCurdOperation_RowCancelingEdit" OnRowEditing="gvDetailsForCurdOperation_RowEditing" OnRowUpdating="gvDetailsForCurdOperation_RowUpdating" OnRowDeleting="gvDetailsForCurdOperation_RowDeleting" OnRowCommand="gvDetailsForCurdOperation_RowCommand" Height="377px" Width="759px" CssClass="auto-style1">  
+  
+                <HeaderStyle CssClass="headerstyleForGrid" />  
+  
+                <Columns>  
+  
+                    <asp:BoundField DataField="CollageId" HeaderText="Collage Id" ReadOnly="true" >  
+  
+                    <FooterStyle HorizontalAlign="Center" />
+                    <ItemStyle HorizontalAlign="Center" />
+                    </asp:BoundField>
+  
+                    <asp:TemplateField HeaderText="Collage Name">  
+  
+                        <ItemTemplate>  
+  
+                            <asp:Label ID="lblCollagename" runat="server" Text='<%# Eval("Collagename")%>' />  
+  
+                        </ItemTemplate>  
+  
+                        <EditItemTemplate>  
+  
+                            <asp:TextBox ID="txtCollagename" runat="server" Text='<%# Eval("Collagename")%>' />  
+  
+                        </EditItemTemplate>  
+  
+                        <FooterTemplate>  
+  
+                            <asp:TextBox ID="txtpname" runat="server" placeholder="Please Enter College name " />  
+                            
+                        </FooterTemplate>  
+  
+                        <ItemStyle HorizontalAlign="Center" />
+  
+                    </asp:TemplateField>  
+  
+                    <asp:TemplateField HeaderText="Collage Rank">  
+  
+                        <ItemTemplate>  
+  
+                            <asp:Label ID="lblCollageRank" runat="server" Text='<%# Eval("CollageRank")%>'></asp:Label>  
+  
+                        </ItemTemplate>  
+  
+                        <EditItemTemplate>  
+  
+                            <asp:TextBox ID="txtCollageRank" runat="server" Text='<%# Eval("CollageRank")%>' />  
+  
+                        </EditItemTemplate>  
+ 
+                        <FooterTemplate>  
+                        <asp:TextBox ID="txtprice" runat="server" placeholder="Please Enter College Rank"/>  
+                           <%-- <asp:TextBox ID="txtCollageRank" runat="server" />  --%>
+  
+                            <asp:Button ID="btnAddNewItem" CommandName="AddNew" runat="server" Text="Add" />  
+  
+                        </FooterTemplate>  
+  
+                        <ItemStyle HorizontalAlign="Center" />
+  
+                    </asp:TemplateField>  
+  
+                    <asp:CommandField ShowEditButton="True" ShowDeleteButton="true" >  
+  
+                    <ItemStyle HorizontalAlign="Center" />
+                    </asp:CommandField>
+  
+                </Columns>  
+  
+            </asp:GridView>  
+  
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  
+  
+            <asp:Label ID="lblresult" runat="server"></asp:Label>  
+  
+        </div>  
+  
+    </form>  
+  
+</body>  
+  
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CollegeRegistrationWithCruDStoredProcedure
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridview();
+            }
+        }
+        protected void BindGridview()
+        {
+            DataSet ds = new DataSet();
+            using (SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=CollegeDB;Integrated Security=True"))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("CrudOperationsInGridView", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@status", "SELECT");
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+                con.Close();
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    gvDetailsForCurdOperation.DataSource = ds;
+                    gvDetailsForCurdOperation.DataBind();
+                }
+                else
+                {
+                    ds.Tables[0].Rows.Add(ds.Tables[0].NewRow());
+                    gvDetailsForCurdOperation.DataSource = ds;
+                    gvDetailsForCurdOperation.DataBind();
+                    int columncount = gvDetailsForCurdOperation.Rows[0].Cells.Count;
+                    gvDetailsForCurdOperation.Rows[0].Cells.Clear();
+                    gvDetailsForCurdOperation.Rows[0].Cells.Add(new TableCell());
+                    gvDetailsForCurdOperation.Rows[0].Cells[0].ColumnSpan = columncount;
+                    gvDetailsForCurdOperation.Rows[0].Cells[0].Text = "No Records Found";
+                }
+            }
+        }
+        protected void gvDetailsForCurdOperation_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName.Equals("AddNew"))
+            {
+                TextBox txtpname = (TextBox)gvDetailsForCurdOperation.FooterRow.FindControl("txtpname");
+                TextBox txtprice = (TextBox)gvDetailsForCurdOperation.FooterRow.FindControl("txtprice");
+                crudoperationsForGrid("INSERT", txtpname.Text, txtprice.Text, 0);
+            }
+        }
+        protected void gvDetailsForCurdOperation_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvDetailsForCurdOperation.EditIndex = e.NewEditIndex;
+            BindGridview();
+        }
+        protected void gvDetailsForCurdOperation_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvDetailsForCurdOperation.EditIndex = -1;
+            BindGridview();
+        }
+        protected void gvDetailsForCurdOperation_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gvDetailsForCurdOperation.PageIndex = e.NewPageIndex;
+            BindGridview();
+        }
+        protected void gvDetailsForCurdOperation_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int CollageId = Convert.ToInt32(gvDetailsForCurdOperation.DataKeys[e.RowIndex].Values["CollageId"].ToString());
+            TextBox txtname = (TextBox)gvDetailsForCurdOperation.Rows[e.RowIndex].FindControl("txtCollagename");
+            TextBox txtCollageRank = (TextBox)gvDetailsForCurdOperation.Rows[e.RowIndex].FindControl("txtCollageRank");
+            crudoperationsForGrid("UPDATE", txtname.Text, txtCollageRank.Text, CollageId);
+        }
+        protected void gvDetailsForCurdOperation_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int CollageId = Convert.ToInt32(gvDetailsForCurdOperation.DataKeys[e.RowIndex].Values["CollageId"].ToString());
+            string Collagename = gvDetailsForCurdOperation.DataKeys[e.RowIndex].Values["Collagename"].ToString();
+            crudoperationsForGrid("DELETE", Collagename, "", CollageId);
+        }
+        protected void crudoperationsForGrid(string status, string Collagename, string price, int CollageId)
+        {
+            using (SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=CollegeDB;Integrated Security=True"))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("CrudOperationsInGridView", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                if (status == "INSERT")
+                {
+                    cmd.Parameters.AddWithValue("@status", status);
+                    cmd.Parameters.AddWithValue("@Collagename", Collagename);
+                    cmd.Parameters.AddWithValue("@CollageRank", price);
+                }
+                else if (status == "UPDATE")
+                {
+                    cmd.Parameters.AddWithValue("@status", status);
+                    cmd.Parameters.AddWithValue("@Collagename", Collagename);
+                    cmd.Parameters.AddWithValue("@CollageRank", price);
+                    cmd.Parameters.AddWithValue("@CollageId", CollageId);
+                }
+                else if (status == "DELETE")
+                {
+                    cmd.Parameters.AddWithValue("@status", status);
+                    cmd.Parameters.AddWithValue("@CollageId", CollageId);
+                }
+                cmd.ExecuteNonQuery();
+                lblresult.ForeColor = Color.Green;
+                lblresult.Text = Collagename + " details " + status.ToLower() + "d successfully";
+                gvDetailsForCurdOperation.EditIndex = -1;
+                BindGridview();
+            }
+        }
+    }
+}
